c30d52ba65c7e298278075a093049a967835bcca
Brett Schwartz <bschwartz92@gmail.com>
2021-07-30 16:54:00 -0400

    update links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,17 +7,17 @@ function Footer() {
         <div className="footer__content__left">
           <div className="links company">
             <header>League</header>
-            <a href="/">Analysis</a>
-            <a href="/">Teams</a>
-            <a href="/">Players</a>
-            <a href="/">Stats</a>
-            <a href="/">Depth Charts</a>
+            <a href="">Analysis</a>
+            <a href="">Teams</a>
+            <a href="">Players</a>
+            <a href="">Stats</a>
+            <a href="">Depth Charts</a>
           </div>
           <div className="links social">
             <header>Company</header>
-            <a href="/">Contact Us</a>
-            <a href="/">Privacy Policy</a>
-            <a href="/">Terms of Use</a>
+            <a href="">Contact Us</a>
+            <a href="">Privacy Policy</a>
+            <a href="">Terms of Use</a>
           </div>
         </div>
         <div className="footer__content__right">
